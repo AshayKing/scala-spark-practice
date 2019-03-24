f62a2e8d7d9aae43147346350f902733dc21c15c
ashay.patil <ashay.patil@pubmatic.com>
2019-03-24 10:21:56 +0530

    order rdd

--- src/main/scala/io/github/ashayking/spark/RetailExample.scala
+++ src/main/scala/io/github/ashayking/spark/RetailExample.scala
@@ -15,19 +15,32 @@ object RetailExample {
     val orderItems = sc.textFile(fileName)
 
     // ACTION1
-    println(orderItems.count())
+    // println(orderItems.count())
 
     // ACTION2
-    println(orderItems.first())
+    // println(orderItems.first())
 
     // ACTION3
-    val orderItemSubTotals = orderItems.map(rec => rec.split(",")(4).toFloat)
+    // val orderItemSubTotals = orderItems.map(rec => rec.split(",")(4).toFloat)
 
     // Lets do sum
     // way1
-    println(orderItemSubTotals.sum())
+    // println(orderItemSubTotals.sum())
     // way2
-    val sum = orderItemSubTotals.reduce((tot, ele) => tot + ele)
-    println(sum)
+    // val sum = orderItemSubTotals.reduce((tot, ele) => tot + ele)
+    // println(sum)
+
+    val orderFileName = "/home/ashay/DBDOCKER/data/retail_db/orders"
+    val orders = sc.textFile(orderFileName)
+
+    println(orders.first())
+
+    //fetching COMPLETED orders
+    orders.filter(rec => rec.split(",")(3) == "COMPLETE")
+      .map(rec => (rec.split(",")(1), 1))
+      //.groupBy(rec => rec._1)
+      .reduceByKey(_ + _)
+      .take(5)
+      .foreach(println)
   }
 }
