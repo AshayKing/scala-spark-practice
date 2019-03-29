f19087b339e59a85baa38ea4a737470c2fde3341
ashay.patil <ashay.patil@pubmatic.com>
2019-03-29 10:42:50 +0530

    trained model

--- src/main/scala/io/github/ashayking/spark/sql/ml/customerchurn/CustomerChurnClassification.scala
+++ src/main/scala/io/github/ashayking/spark/sql/ml/customerchurn/CustomerChurnClassification.scala
@@ -2,6 +2,13 @@ package io.github.ashayking.spark.sql.ml.customerchurn
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Dataset
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.tuning.ParamGridBuilder
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.tuning.CrossValidator
 
 /**
  * @author Ashay S Patil
@@ -31,9 +38,9 @@ object CustomerChurnClassification {
       .csv(trainFilePath)
       .as[Account]
 
-    train.take(1)
+    //train.take(1)
     train.cache
-    println(train.count)
+    //println(train.count)
 
     // Testing Dataset
     val test: Dataset[Account] = spark
@@ -41,14 +48,87 @@ object CustomerChurnClassification {
       .option("inferSchema", "false")
       .schema(Models.schema)
       .csv(testFilePath).as[Account]
-    
-    test.take(2)
+
+    //test.take(2)
     test.cache
-    println(test.count)
+    //println(test.count)
 
     // Printing Schema & Some data
-    train.printSchema()
-    train.show
+    //train.printSchema()
+    //train.show
+
+    // Caching table
+    train.createOrReplaceTempView("account")
+    spark.catalog.cacheTable("account")
+
+    // Printing results with respect to label
+    train.groupBy("churn").count.show
+
+    // It is unbalanced dataset so selected only fraction of data
+    val fractions = Map("False" -> .17, "True" -> 1.0)
+    val strain = train.stat.sampleBy("churn", fractions, 36L)
+
+    strain.groupBy("churn").count.show
+
+    // Removing non-required columns
+    val ntrain = strain
+      .drop("state")
+      .drop("acode")
+      .drop("vplan")
+      .drop("tdcharge")
+      .drop("techarge")
+    println(ntrain.count)
+    ntrain.show
+
+    // handling non-numeric variables
+    val ipindexer = new StringIndexer()
+      .setInputCol("intlplan")
+      .setOutputCol("iplanIndex")
+
+    val labelindexer = new StringIndexer()
+      .setInputCol("churn")
+      .setOutputCol("label")
+
+    val featureCols = Array("len", "iplanIndex", "numvmail", "tdmins", "tdcalls", "temins", "tecalls", "tnmins", "tncalls", "timins", "ticalls", "numcs")
+
+    // Crearting vector assembler
+    val assembler = new VectorAssembler()
+      .setInputCols(featureCols)
+      .setOutputCol("features")
+
+    // ML model to try out
+    val dTree = new DecisionTreeClassifier().setLabelCol("label")
+      .setFeaturesCol("features")
+
+    /// Pipelines of Transformations
+    val pipeline = new Pipeline().setStages(Array(ipindexer, labelindexer, assembler, dTree))
+
+    // Hyperparam Tuning
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(dTree.maxDepth, Array(2, 3, 4, 5, 6, 7))
+      .build()
+
+    val evaluator = new BinaryClassificationEvaluator()
+      .setLabelCol("label")
+      .setRawPredictionCol("prediction")
+
+    // Set up 3-fold cross validation
+    val crossval = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(3)
+
+    val cvModel = crossval.fit(ntrain)
+
+    // Getting Best Model
+    val bestModel = cvModel.bestModel
+    println("The Best Model and Parameters:\n--------------------")
+    println(bestModel.asInstanceOf[org.apache.spark.ml.PipelineModel].stages(3))
+    //bestModel.asInstanceOf[org.apache.spark.ml.PipelineModel]
+    //  .stages(3)
+    //  .extractParamMap
+
   }
 
 }
